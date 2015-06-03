ea79c86cd5c2a566c845241a2c24c0dfe4384348
Warren Strange <warren.strange@gmail.com>
2015-06-03 11:30:25 -0600

    updated sh, .gitignore

--- openam-nightly/run-openam.sh
+++ openam-nightly/run-openam.sh
@@ -5,10 +5,16 @@
 
 mkdir -p /root/.openamcfg
 cat >/root/.openamcfg/AMConfig_usr_local_tomcat_webapps_openam_ <<HERE
-/root/openam/openam01
+/root/openam
 HERE
 
+# Todo - create the bootstrap file, and any other directories needed
+# This should be done via ENV vars?
 
+
+# Suggestion from Victor
+#CATALINA_OPTS="-Xms128m -Xmx1024m -XX:MaxPermSize=256m -D\"com.sun.identity.configuration.directory=/root/openam/\" -D\"bootstrap.dir=/root/openam\""
+# export CATALINA_OPTS
 cd /usr/local/tomcat 
 bin/catalina.sh run 
 
