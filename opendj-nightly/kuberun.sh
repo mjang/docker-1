6390eb661e5f3b2148fcd0288839812c8c30aabe
Warren Strange <warren.strange@gmail.com>
2015-10-28 12:39:13 -0700

    Consolidate writable dirs to instance

---
+++ opendj-nightly/kuberun.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# This startup script is meant to be used in a Kubernetes environment
+# The idea is to consolidate all of the writable DJ diretories to
+# a single directory root, and update DJ' instance.loc file to point there
+# Then we can hae k8 mount a data volume on that root - to give us
+# persistence across restarts of OpenDJ
+
+# Instance dir does not exist?
+if [ ! -d instance ] ; then
+  mkdir instance
+  mv config/ db/  instance
+  mkdir instance/logs
+  mkdir instance/locks
+  echo "./instance" > instance.loc
+fi
+
+bin/start-ds
+
+tail -f instance/logs/server.out
