64e6464bb49179e1d6021336738bf86eec31de3c
Warren Strange <warren.strange@gmail.com>
2015-10-28 14:28:43 -0700

    Simplify openam build

--- openam-nightly/run-openam.sh
+++ openam-nightly/run-openam.sh
@@ -1,20 +1,14 @@
 #!/bin/sh
+# Start OpenAM
+# To create a persistent configuration mount a data volume on /openam/root
 
-# Fix up openam to point to personality ldap
-# TODO: figure out how to get different nodes 
-
+#  Point instance dir at /root/openam
 mkdir -p /root/.openamcfg
 cat >/root/.openamcfg/AMConfig_usr_local_tomcat_webapps_openam_ <<HERE
 /root/openam
 HERE
 
-# Todo - create the bootstrap file, and any other directories needed
-# This should be done via ENV vars?
 
-
-# Suggestion from Victor
-#CATALINA_OPTS="-Xms128m -Xmx1024m -XX:MaxPermSize=256m -D\"com.sun.identity.configuration.directory=/root/openam/\" -D\"bootstrap.dir=/root/openam\""
-# export CATALINA_OPTS
 cd /usr/local/tomcat 
 bin/catalina.sh run 
 
