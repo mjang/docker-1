4c52567c1bed6fd26bd91499b5844cb7ce250285
Warren Strange <warren.strange@gmail.com>
2015-11-05 10:21:00 -0700

    Revert 07d306703c31a1a4b478ab7d5fd3b248937f1de2

--- opendj-nightly/run.sh
+++ opendj-nightly/run.sh
@@ -8,56 +8,13 @@
 
 cd /opt/opendj
 
-if [ ! -d bin ] ; then
-
-  if [ ! -e /installers/opendj.zip ] ; then
-    curl https://forgerock.org/djs/opendjrel.js?948497823 | grep -o "http://.*\.zip" | tail -1 | xargs curl -o /opt/opendj.zip;
-    if [ -w /installers/ ] ; then
-      cp /opt/opendj.zip /installers/
-    fi
-  else
-    cp /installers/opendj.zip /opt/opendj.zip
-  fi
-
-  if [ ! -e response_file.props ] ; then
-    cat > response_file.props <<EOF
-#
-# Sample properties file to set up OpenDJ directory server
-#
-hostname                        =localhost
-ldapPort                        =389
-generateSelfSignedCertificate   =true
-enableStartTLS                  =true
-ldapsPort                       =636
-jmxPort                         =689
-adminConnectorPort              =4444
-rootUserDN                      =cn=Directory Manager
-rootUserPassword                =password
-baseDN                          =dc=example,dc=com
-sampleData                      =100
-doNotStart                      =true
-EOF
-
-  fi
-
-  unzip /opt/opendj.zip -d /opt && /opt/opendj/setup --cli --propertiesFilePath response_file.props --acceptLicense --no-prompt \
-      && /opt/opendj/bin/stop-ds && rm /opt/opendj.zip
-
-  sleep 5
-fi
-
-cd /opt/opendj
-
-# have config dir not been moved?
-if [ -d config ] ; then
-  mkdir -p instance/logs
-  mkdir -p instance/locks
-  mkdir -p instance/db
-  mkdir -p instance/config
-
-  mv config/* instance/config/
-  mv db/* instance/db/
-
+# Instance dir does not exist?
+if [ ! -d instance ] ; then
+  mkdir instance
+  mv config/ instance
+  mv db/ instance
+  mkdir instance/logs
+  mkdir instance/locks
   echo "./instance" > instance.loc
 fi
 
