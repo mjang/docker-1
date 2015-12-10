82caed525716fc7c75e21f9e5702510dc40165f3
Warren Strange <warren.strange@gmail.com>
2015-12-10 15:12:43 -0400

    DJ updates

--- opendj-nightly/README.md
+++ opendj-nightly/README.md
@@ -1,21 +1,23 @@
-ForgeRock OpenDJ nightly build
+# ForgeRock OpenDJ nightly build
 
 Listens on 389/636/4444
 
-Default password is 'password'
+Default bind credentials are CN=Directory Manager, password is 'password'
 
-All writable directories (persisted data) are collected up under /opt/opendj/instance/instance1
+All writable directories (persisted data) are collected up under /opt/opendj/instances/instance1
 
 To run with Docker (example)
 ```
 mkdir dj    # Make an instance dir to perist data
-docker run -i -t -v `pwd`/dj:/opt/opendj/instance wstrange/opendj-nightly:latest
+docker run -i -t -v `pwd`/dj:/opt/opendj/instances/instance1 wstrange/opendj-nightly:latest
 ```
 
 For Kubernetes mount a PV on /opt/opendj/instance/instance1
 
-If you choose not to mount a persistent volume OpenDJ will start 
+If you choose not to mount a persistent volume OpenDJ will start OK - but you will lose your data when the container is removed.
 
-TODO: 
-Create a strategy for changing the password.  If a secrets volume is present (k8), use that to set the DJ password.
+
+# TODO: 
+Create a strategy for changing the password.  If a secrets volume is present (k8), use that to set the DJ password?
+
 
--- opendj-nightly/run.sh
+++ opendj-nightly/run.sh
@@ -2,24 +2,21 @@
 # Run the OpenDJ server
 # The idea is to consolidate all of the writable DJ directories to
 # a single instance directory root, and update DJ's instance.loc file to point to that root
-# This allows us to to mount a data volume on that root which will give us
-# persistence across restarts of OpenDJ. For Docker - mount a data volume on /opt/opendj/instance. 
+# This allows us to to mount a data volume on that root which  gives us
+# persistence across restarts of OpenDJ. 
+# For Docker - mount a data volume on /opt/opendj/instances/instance1. 
 # For Kubernetes mount a PV
+# To "prime" the sytem the first time DJ is run, we copy in a skeleton 
+# DJ instance from the instances/template directory that was created in the Dockerfile
 
 cd /opt/opendj
 
 # Instance dir does not exist?
-if [ ! -d instance/config ] ; then
-  # Consolidate 
-  mkdir -p instance
-  mv config/ instance
-  mv db/ instance
-  mkdir instance/logs
-  mkdir instance/locks
-  cp -r lib/extensions/ instance/extensions 
-  echo "./instance" > instance.loc
+if [ ! -d instances/instance1/config ] ; then
+  # Copy the template
+  echo Instance Directory is empty. Creating new instance from template
+  cp -r instances/template/* instances/instance1
+  echo "./instances/instance1" > instance.loc
 fi
 
-./bin/start-ds
-
-tail -f instance/logs/server.out
+./bin/start-ds --nodetach
