4b87751cf671416b362667321dd05131fa7ad515
Warren Strange <warren.strange@gmail.com>
2016-01-11 17:44:34 -0700

    reworked ssoconfig to use mounted volume

--- ssoconfig-nightly/README.md
+++ ssoconfig-nightly/README.md
@@ -1,8 +1,16 @@
 
 
-This is a docker image that runs the OpenAM config tool to configure a freshly installed 
-instance. The file master.properties is used to configure the server.
+# ssoconfig
 
+This is a docker image that runs the OpenAM config tool to configure a freshly installed
+instance.
 
-todo: Make this more generic...
+The image expects that a volume /config/openam/ will be mounted and contains
+property files that will be applied. The property files will be applied in
+lexicalgraphic order (01_file1.properties, etc.).
 
+If /config/openam does not exist, a default configuration will be used. It is
+probably not what you want.
+
+In a Kubernetes environment /config/openam should probably come from a
+gitRepo volume.
--- ssoconfig-nightly/config.sh
+++ ssoconfig-nightly/config.sh
@@ -2,20 +2,33 @@
 
 cd /var/tmp
 
-# Optionally pass in URL of OpenAM server
+if[ ! -d /config/openam ]; then
+		echo "Can't find configuration directory /config/openam. Using default config"
+		mkdir -p /config/openam
+		cp master.properties /config/openam
+fi
 
-URL=${OPENAM_URL:-"http://openam-svc-a:80/openam"}
 
-T="$URL/config/options.htm"
-
-echo Configuring OpenAM $URL 
-
-until $(curl --output /dev/null --silent --head --fail $T); do
-	echo "Waiting for OpenAM server at $URL "
-    sleep 5
+for file in /config/openam/*.properties do
+		echo "Applying property file $file"
+		# This is kind of a kludge. The props file is not 100% valid shell
+		# so you will see an error -but it serves to set the SERVER_URL arg
+		# we need
+		source $file
+		wait_for_openam_up $SERVER_URL
+		echo "Running Configurator"
+		java -jar openam-configurator-tool*.jar -f $file
 done
-# Sleep an additonal 10 in case DJ is not quite up yet
-sleep 10
 
-java -jar openam-configurator-tool*.jar -f master.properties
 
+function wait_for_openam_up
+{
+	T="$1/config/options.htm"
+
+	until $(curl --output /dev/null --silent --head --fail $T); do
+		echo "Waiting for OpenAM server at $T "
+    sleep 5
+	done
+	# Sleep an additonal 10 in case DJ is not quite up yet
+	sleep 10
+}
