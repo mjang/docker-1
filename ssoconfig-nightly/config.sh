5415d0d43fb5b3ca964c4f02b8401be2cc8112a4
Warren Strange <warren.strange@gmail.com>
2016-01-11 18:24:57 -0700

    fixes config.sh

--- ssoconfig-nightly/config.sh
+++ ssoconfig-nightly/config.sh
@@ -1,27 +1,17 @@
 #!/usr/bin/env bash
 
-cd /var/tmp
+#cd /var/tmp
 
-if[ ! -d /config/openam ]; then
+CONFIG=/config/openam
+
+if [ ! -d /config/openam ]; then
 		echo "Can't find configuration directory /config/openam. Using default config"
-		mkdir -p /config/openam
-		cp master.properties /config/openam
+		mkdir -p $CONFIG
+		cp default.properties $CONFIG
 fi
 
 
-for file in /config/openam/*.properties do
-		echo "Applying property file $file"
-		# This is kind of a kludge. The props file is not 100% valid shell
-		# so you will see an error -but it serves to set the SERVER_URL arg
-		# we need
-		source $file
-		wait_for_openam_up $SERVER_URL
-		echo "Running Configurator"
-		java -jar openam-configurator-tool*.jar -f $file
-done
-
-
-function wait_for_openam_up
+function wait_for_openam
 {
 	T="$1/config/options.htm"
 
@@ -32,3 +22,17 @@ function wait_for_openam_up
 	# Sleep an additonal 10 in case DJ is not quite up yet
 	sleep 10
 }
+
+
+
+for file in $CONFIG/*.properties
+do
+		echo "Applying property file $file"
+		# This is kind of a kludge. The props file is not 100% valid shell
+		# so you will see an error -but it serves to set the SERVER_URL arg
+		# we need
+		source $file
+		wait_for_openam   $SERVER_URL
+		echo "Running Configurator"
+		java -jar openam-configurator-tool*.jar -f $file
+done
