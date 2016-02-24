abe45105344b88dec09fb01d3b615e2e207098e3
Warren Strange <warren.strange@gmail.com>
2016-02-24 15:26:38 -0800

    IG updates

--- openig/run-openig.sh
+++ openig/run-openig.sh
@@ -2,7 +2,7 @@
 # Start OpenIG enabled Tomcat
 # To create a persistent configuration mount a data volume on /root/config
 
-mkdir -p /root/config
+mkdir -p /root/openig
 # Trigger the OpenIG WAR deployment by removing the default ROOT app
 rm -rf /usr/local/tomcat/webapps/ROOT
 # Start Tomcat
