710022dc221019b70df1e47787e61e8c624785a6
Warren Strange <warren.strange@gmail.com>
2016-02-24 15:53:59 -0800

    IG fixes

--- openig/run-openig.sh
+++ openig/run-openig.sh
@@ -3,7 +3,6 @@
 # To create a persistent configuration mount a data volume on /root/config
 
 mkdir -p /root/openig
-# Trigger the OpenIG WAR deployment by removing the default ROOT app
-rm -rf /usr/local/tomcat/webapps/ROOT
+
 # Start Tomcat
 /usr/local/tomcat/bin/catalina.sh run
